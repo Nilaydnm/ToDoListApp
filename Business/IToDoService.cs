4229214e04e09287c328f39ad6c561b588b27d83
Nilaydnm <nilaydonmez390@gmail.com>
2025-07-22 10:16:19 +0300

    Ninject ile Dependency Injection yapıldı. Interface ve bussiness katmanı eklendi.

---
+++ Business/IToDoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entities;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace Business
+{
+    public interface IToDoService
+    {
+        List<ToDo> GetAll();
+        ToDo GetById(int id);
+        void Add(ToDo todo);
+        void Update(ToDo todo);
+        void Delete(int id);
+    }
+}
---
+++ Business/ToDoManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using DataAccess;
+using Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class ToDoManager : IToDoService
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoManager(ToDoContext context)
+        {
+            _context = context;  // Dependency injection ile ToDoContext'i alıyoruz
+        }
+
+        public void Add(ToDo todo)
+        {
+            _context.ToDos.Add(todo);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var todo = _context.ToDos.Find(id);
+            if (todo != null)
+            {
+                _context.ToDos.Remove(todo);
+                _context.SaveChanges();
+            }
+        }
+
+        public List<ToDo> GetAll()
+        {
+            return _context.ToDos.OrderByDescending(x => x.CreatedAt).ToList();
+        }
+
+        public ToDo GetById(int id)
+        {
+            return _context.ToDos.Find(id);
+        }
+
+        public void Update(ToDo todo)
+        {
+            var entity = _context.ToDos.Find(todo.Id);
+            if (entity != null)
+            {
+                entity.Title = todo.Title;
+                entity.IsCompleted = todo.IsCompleted;
+                _context.SaveChanges();
+            }
+        }
+    }
+}
